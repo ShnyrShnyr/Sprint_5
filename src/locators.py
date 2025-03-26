8b393a3502169e4b11f6feea7a25fd68155a7e27
ShnyrShnyr <aas280883@gmail.com>
2025-03-26 14:38:40 +0400

    Fourth commit with changes after review

--- src/locators.py
+++ src/locators.py
@@ -2,14 +2,14 @@ from selenium.webdriver.common.by import By
 
 class RegLocators: # страница регистрации
     NAME_FIELD = By.XPATH, "//label[text()='Имя']/../input" # поле ввода Имени
-    EMAIL_FIELD = By.XPATH, "//fieldset[2]/div/div/input" # поле ввода email
-    PASS_FIELD = By.XPATH, "//fieldset[3]/div/div/input" # поле ввода пароля
+    EMAIL_FIELD = By.XPATH, "//label[text()='Email']/../input" # поле ввода email
+    PASS_FIELD = By.XPATH, "//label[text()='Пароль']/../input" # поле ввода пароля
     REG_BUTTON = By.XPATH, "//form/button" # кнопка Зарегистрироваться
     ENTER_TEXT = By.XPATH, "//p/a" # кнопка Войти
 
 class SignInLocators: # страница авторизации
     EMAIL_FIELD = By.XPATH, "//label[text()='Email']/../input" # поле ввода email
-    PASS_FIELD = By.XPATH, "//fieldset[2]/div/div/input"  # поле ввода пароля
+    PASS_FIELD = By.XPATH, "//label[text()='Пароль']/../input"  # поле ввода пароля
     WRONG_PASS = By.XPATH,"//fieldset/div/p" # сообщение о неверном пароле
     COME_IN_BUTTON = By.XPATH, "//form/button" # кнопка Войти
     ENTRANCE_HEADER = By.XPATH, "//main/div/h2" # заголовок вход
@@ -17,16 +17,16 @@ class SignInLocators: # страница авторизации
 
 class MainPageLocators: # главная страница
     ENTER_BUTTON = By. XPATH, "//nav/a" # кнопка войти в аккаунт
-    ORDER_BUTTON = By.XPATH, "//main/section/div/button" # кнопка оформить заказ
-    CONSTRUCTOR_BUTTON = By.XPATH, "//li[1]/a"  # кнопка конструктор
+    ORDER_BUTTON = By.XPATH, "//button[text()='Войти в аккаунт']" # кнопка оформить заказ
+    CONSTRUCTOR_BUTTON = By.XPATH, "//a[.//p[text()='Конструктор']]"  # кнопка конструктор
     HEADER_OF_CONSTRATOR = By.XPATH, "//h1" # заголовок Соберите бургер
     LOGO = By.XPATH, "//div/a" # логотип в хедерах
-    BUNS_TEXT = By.XPATH, "//section/div/h2[1]" # текст булки
-    SAUSES_TEXT = By.XPATH, "//section/div/h2[2]"  # текст Соусы
-    FILLINGS_TEXT = By.XPATH, "//section/div/h2[3]"  # текст Начинки
-    BUNS_MENU = By.XPATH, "//main/section[1]/div[1]/div[1]" # заголовок булки
-    SAUSES_MENU = By.XPATH, "//main/section[1]/div[1]/div[2]" # заголовок соусы
-    FILLINGS_MENU = By.XPATH, "//main/section[1]/div[1]/div[3]" # заголовок начинки
+    BUNS_TEXT = By.XPATH, "//h2[text()='Булки']" # текст булки
+    SOUSES_TEXT = By.XPATH, "//h2[text()='Соусы']"  # текст Соусы
+    FILLINGS_TEXT = By.XPATH, "//h2[text()='Начинки']"  # текст Начинки
+    BUNS_MENU = By.XPATH, "//div[contains(@class, 'current')]" # заголовок булки кликабельный выбранный
+    SOUSES_MENU = By.XPATH, "//span[text()='Соусы']" # заголовок соусы кликабельный
+    FILLINGS_MENU = By.XPATH, "//span[text()='Начинки']" # заголовок начинки кликабельный
 
 
 class PersonalAccount:
--- tests/test_menu.py
+++ tests/test_menu.py
@@ -6,14 +6,21 @@ from src.locators import MainPageLocators
 
 
 class TestMenu:
-    def test_menu(self, driver):
-        driver.find_element(*MainPageLocators.BUNS_TEXT).click()
-        buns = WebDriverWait(driver,Config.TIMEOUT).until(ec.visibility_of_element_located(MainPageLocators.BUNS_MENU))
-        driver.find_element(*MainPageLocators.SAUSES_TEXT).click()
-        sauses = WebDriverWait(driver, Config.TIMEOUT).until(ec.visibility_of_element_located(MainPageLocators.SAUSES_MENU))
-        driver.find_element(*MainPageLocators.FILLINGS_TEXT).click()
-        fillings = WebDriverWait(driver,Config.TIMEOUT).until(ec.visibility_of_element_located(MainPageLocators.FILLINGS_MENU))
-        assert buns.is_displayed(), "Failed BULKI click"
-        assert sauses.is_displayed(), "Failed SOUSES click"
-        assert fillings.is_displayed(), "Failed NACHINKI click"
+    def test_menu_scroll_buns(self, driver):
+        driver.find_element(*MainPageLocators.CONSTRUCTOR_BUTTON).click()
+        driver.find_element(*MainPageLocators.BUNS_MENU)
+        buns = WebDriverWait(driver,Config.TIMEOUT).until(ec.visibility_of_element_located(MainPageLocators.BUNS_TEXT))
+        driver.execute_script("arguments[0].scrollIntoView();", buns)
+        assert "Failed BUNS click"
 
+    def test_menu_scroll_souses(self, driver):
+        driver.find_element(*MainPageLocators.SOUSES_MENU).click()
+        souses = WebDriverWait(driver,Config.TIMEOUT).until(ec.visibility_of_element_located(MainPageLocators.SOUSES_TEXT))
+        driver.execute_script("arguments[0].scrollIntoView();", souses)
+        assert "Failed SOUSES click"
+
+    def test_menu_scroll_fillings(self, driver):
+        driver.find_element(*MainPageLocators.FILLINGS_MENU).click()
+        fillings = WebDriverWait(driver,Config.TIMEOUT).until(ec.visibility_of_element_located(MainPageLocators.FILLINGS_TEXT))
+        driver.execute_script("arguments[0].scrollIntoView();", fillings)
+        assert "Failed FILLINGS click"
